c80137dbc96e9a8da1640a6a82582c0373fe87eb
Yiwen-Liu-Authright <yiwen.liu@authright.com>
2019-11-04 11:47:31 -0500

    Auth middleware & JWT verify

---
+++ middleware/auth.js
@@ -0,0 +1,22 @@
+const jwt = require("jsonwebtoken");
+const config = require("config");
+
+// next is the call back
+module.exports = function(request, response, next) {
+  // Get the token from the header
+  const token = request.header("x-auth-token");
+
+  // Check if no token
+  if (!token) {
+    return response.status(401).json({ msg: "No token, authorization denied" });
+  }
+
+  try {
+    const decoded = jwt.verify(token, config.get("jwtSecret"));
+
+    request.user = decoded.user;
+    next();
+  } catch (error) {
+    response.status(401).json({ msg: "Token is not valid" });
+  }
+};
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,9 +1,19 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const auth = require("../../middleware/auth");
 
+const User = require("../../models/User");
 // @route   GET api/auth
 // @desc    Test
 // @access  Public
-router.get('/', (request, response) => response.send('auth route...'));
+router.get("/", auth, async (request, response) => {
+  try {
+    const user = await User.findById(request.user.id).select("-password");
+    response.json(user);
+  } catch (error) {
+    console.error(error.message);
+    response.status(500).send("Server Error");
+  }
+});
 
 module.exports = router;
